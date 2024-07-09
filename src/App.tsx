2ba72da1d6dc7bd8e788bc368d021e24c6a80dcc
rsbqr <rsbqr.coder@gmail.com>
2024-07-10 01:44:40 +0700

    dashboard

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Portofolio</h1>
+      <div>
+        <a href="https://halaman-data.streamlit.app/Bike_Data_Demo" target="_blank" rel="Projek Dashboard Data Analisis">
+          <button>
+            Dashboard Data Sepeda
+          </button>
+        </a>
+      </div>
+
+      <div>
+        <a href="https://halaman-data.streamlit.app/Bike_Data_Demo" target="_blank" rel="Projek Dashboard Data Analisis">
+          <button>
+            Dashboard Data Sepeda
+          </button>
+        </a>
+      </div>
+
+      <div>
+        <a href="https://halaman-data.streamlit.app/Bike_Data_Demo" target="_blank" rel="Projek Dashboard Data Analisis">
+          <button>
+            Dashboard Data Sepeda
+          </button>
+        </a>
+      </div>
+
+      <div>
+        <a href="https://halaman-data.streamlit.app/Bike_Data_Demo" target="_blank" rel="Projek Dashboard Data Analisis">
+          <button>
+            Dashboard Data Sepeda
+          </button>
+        </a>
+      </div>
+
+      <p className="read-the-docs">
+      <h5> Halaman Portofolio Menggunakan</h5>
+        <a href="https://vitejs.dev" target="_blank">
+          <img sizes='14'src={viteLogo} className="logo" alt="Vite" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React" />
+        </a>
+      </p>
+    
+    </>
+  )
+}
+
+export default App
