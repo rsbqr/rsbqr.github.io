c8fbd8c7c5aec1fc7042703282ce337071b4c65f
Raihan Sabiq Rabbani <43677172+rsbqr@users.noreply.github.com>
2024-07-10 01:49:21 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' //main direktori localhost
+import App from './App.tsx' //main direktori localhost
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
